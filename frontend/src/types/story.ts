929c957a48b651761b82dd561d8ae4148e2f80f1
irenee-14 <ireneee145@gmail.com>
2025-09-04 15:18:39 +0900

    choe: story type 추가

--- frontend/src/types/story.ts
+++ frontend/src/types/story.ts
@@ -1,6 +1,8 @@
 export type Flow = {
   id: string;
   title: string;
+  characterName: string;
+  backgroundName: string;
   imgUrl: string;
   tags: string[];
   description: string;
@@ -10,10 +12,14 @@ export type Flow = {
 
 export type Story = {
   storyId: string;
+  name: string;
   characterId: string;
+  characterName: string;
   backgroundId: string;
+  backgroundName: string;
   writerId: string;
   basic: boolean;
+  img: string;
   characterPrompt: string;
   opening: string;
   createdAt: string;
@@ -22,5 +28,5 @@ export type Story = {
 
 export type StoryResponse = {
   success: boolean;
-  data: Flow[] | Story | Story[];
+  data: Flow | Flow[] | Story | Story[];
 };
